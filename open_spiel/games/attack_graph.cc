f7d1cba0365a53c8a25ca6e20bfc141607f3227f
wangyzh <wangyzh@umich.edu>
2020-07-14 21:23:08 -0400

    revert

---
+++ open_spiel/games/attack_graph.cc
@@ -0,0 +1,16 @@
+#include "open_spiel/games/markov_soccer.h"
+
+#include <memory>
+#include <utility>
+
+#include "open_spiel/spiel.h"
+#include "open_spiel/utils/tensor_view.h"
+
+namespace open_spiel {
+namespace attack_graph {
+
+namespace {
+
+}
+}  // namespace attack_graph
+}  // namespace open_spiel
